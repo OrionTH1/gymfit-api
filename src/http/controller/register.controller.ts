20b996b9fc65950cb7f15d8227db9f4d0ea39ef2
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-08-26 00:54:42 -0300

    feat: create a error for create user with email duplicated

--- src/http/controller/register.controller.ts
+++ src/http/controller/register.controller.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users.repository"
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exits.error"
 import { RegisterUseCase } from "@/use-cases/register"
 import { FastifyReply, FastifyRequest } from "fastify"
 import { z } from "zod"
@@ -21,7 +22,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     })
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from "@/repositories/users.repository";
 import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./errors/user-already-exits.error";
 
 type RegisterUseCaseRequest = {
   name: string;
@@ -13,7 +14,7 @@ export class RegisterUseCase {
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const userWithSameEmail = await this.userRepository.findByEmail(email)
 
-    if (userWithSameEmail) throw new Error('Email already exists')
+    if (userWithSameEmail) throw new UserAlreadyExistsError()
 
     const password_hash = await hash(password, 6)
 
