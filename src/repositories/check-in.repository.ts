7334f9c14c755b6f8064c3a75100a4844ec89e72
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-09-14 17:10:15 -0300

    feat: Add method to find many check-ins by user ID

--- src/repositories/check-in.repository.ts
+++ src/repositories/check-in.repository.ts
@@ -2,5 +2,6 @@ import { CheckIn, Prisma } from "generated/prisma";
 
 export interface CheckInRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
 }
