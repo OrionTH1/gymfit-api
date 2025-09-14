3d8151bfe194c80ca5ce3dd686b68312e5204a7b
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-09-14 11:34:04 -0300

    feat: Add validations to check-in use case
    - Validate if the gym exists - Validate if the user is within range of
    the gym

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryCheckInRepository } from '@/repositories/in-memory/check-in.rep
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { CheckInUseCase } from './check-in'
 import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms.repository'
+import { ResourceNotFoundError } from './errors/resource-not-found.error'
 
 let checkInRepository: InMemoryCheckInRepository
 let gymsRepository: InMemoryGymsRepository
@@ -14,6 +15,7 @@ describe('Check In Use Case', () => {
     sut = new CheckInUseCase(checkInRepository, gymsRepository)
 
     gymsRepository.create({
+      id: 'Gym_01',
       title: 'Gym 01',
       description: 'Gym 01',
       phone: '123456789',
@@ -29,25 +31,42 @@ describe('Check In Use Case', () => {
   })
 
   it("should be able to create a check-in", async () => {
-    const { checkIn } = await sut.execute({ gymId: '123', userId: '123', userLatitude: 0, userLongitude: 0 });
+    const { checkIn } = await sut.execute({ gymId: 'Gym_01', userId: '123', userLatitude: 0, userLongitude: 0 });
 
     expect(checkIn.id).toEqual(expect.any(String))
   })
 
   it("should not be able to create a check-in twice in the same day", async () => {
     vi.setSystemTime(new Date())
-    await sut.execute({ gymId: '123', userId: '123', userLatitude: 0, userLongitude: 0 });
+    await sut.execute({ gymId: 'Gym_01', userId: '123', userLatitude: 0, userLongitude: 0 });
 
-    await expect(sut.execute({ gymId: '123', userId: '123', userLatitude: 0, userLongitude: 0 })).rejects.toBeInstanceOf(Error)
+    await expect(sut.execute({ gymId: 'Gym_01', userId: '123', userLatitude: 0, userLongitude: 0 })).rejects.toBeInstanceOf(Error)
   })
 
   it("should be able to create a check-in twice in different days", async () => {
     vi.setSystemTime(new Date(2025, 1, 1))
-    await sut.execute({ gymId: '123', userId: '123', userLatitude: 0, userLongitude: 0 });
+    await sut.execute({ gymId: 'Gym_01', userId: '123', userLatitude: 0, userLongitude: 0 });
 
     vi.setSystemTime(new Date(2025, 1, 2))
-    const { checkIn } = await sut.execute({ gymId: '123', userId: '123', userLatitude: 0, userLongitude: 0 })
+    const { checkIn } = await sut.execute({ gymId: 'Gym_01', userId: '123', userLatitude: 0, userLongitude: 0 })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it("should not be able to check in a inexistent gym", async () => {
+    await expect(sut.execute({ gymId: 'NOT EXIST', userId: '123', userLatitude: 0, userLongitude: 0 })).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it("should not be able to check in on distant gym", async () => {
+    gymsRepository.create({
+      id: "Gym_test",
+      title: 'Gym 01',
+      description: 'Gym 01',
+      phone: '123456789',
+      latitude: -22.7215197,
+      longitude: -43.3584274
+    })
+
+    await expect(sut.execute({ gymId: 'Gym_test', userId: '123', userLatitude: 0, userLongitude: 0 })).rejects.toBeInstanceOf(Error)
   })
 })
