d4a6636473d09703a4bbb69975804bb15ca19909
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-09-14 21:08:48 -0300

    fix: rename CreateGymUseCase import

--- src/use-cases/create-gym.spec.ts
+++ src/use-cases/create-gym.spec.ts
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms.repository'
-import { CreateGymUseCase } from './create-gyms'
+import { CreateGymUseCase } from './create-gym'
 
 let gymsRepository: InMemoryGymsRepository
 let sut: CreateGymUseCase
